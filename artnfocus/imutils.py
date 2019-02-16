95d298a670f49e1c4f74cf331e135585fa199925
T. E. Pickering <te.pickering@gmail.com>
2019-02-15 17:10:06 -0700

    stitch together mont4k images into single frames with E left and N up; add routine to subtract background from mont4k images

--- artnfocus/imutils.py
+++ artnfocus/imutils.py
@@ -5,14 +5,14 @@ from typing import List
 
 import numpy as np
 
-from astropy import stats, wcs
+from astropy import wcs
 from astropy.nddata import CCDData
 
 import photutils
 import ccdproc
 
 
-def ARTNreduce(filename: str) -> List[CCDData]:
+def ARTNreduce(filename: str) -> CCDData:
     reduced = []
     hdus = (1, 2)
     for h in hdus:
@@ -22,4 +22,26 @@ def ARTNreduce(filename: str) -> List[CCDData]:
         im = ccdproc.subtract_overscan(im, fits_section=oscansec, overscan_axis=None)
         im = ccdproc.trim_image(im, fits_section=trimsec)
         reduced.append(im)
-    return reduced
+
+    # hacky, hard-coded way to make combined image for mont4k at 3x3 binning. result is E to left, N up.
+    w = reduced[1].wcs.copy()
+    w.array_shape = (1365, 1364)
+    blank = np.zeros((1365, 1364))
+    blank[:, :682] = reduced[1].data
+    blank[:, 682:] = np.fliplr(reduced[0].data)
+    stitched = CCDData(blank, wcs=w, unit=reduced[0].unit)
+    stitched.data[:, 1088] = (stitched.data[:, 1087] + stitched.data[:, 1089]) / 2.
+
+    return stitched
+
+
+def sub_background(image: CCDData, filter_size: int = 9, box_size: int = 50) -> np.ndarray:
+    bkg_estimator = photutils.MedianBackground()
+    bkg = photutils.Background2D(
+        image,
+        (box_size, box_size),
+        filter_size=(filter_size, filter_size),
+        bkg_estimator=bkg_estimator
+    )
+    sub = image.data - bkg.background
+    return sub
